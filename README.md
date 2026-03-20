a7da05d1f3c1f79f834b03e5a5abdecc1e5536b7
Ben Ye <termina.y.email@gmail.com>
2026-03-20 17:36:31 +0100

    Added frequency-based subsampling to preprocess.py

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Downloaded at http://mattmahoney.net/dc/text8.zip on 15/03/2026 10:58 PM, though
 https://dumps.wikimedia.org/other/mediawiki_content_current/enwiki/2026-03-01/xml/bzip2/enwiki-2026-03-01-p10p1147431.xml.bz2 on 15/03/2026 11:04 PM, though it will not probably change
 
 ```bash
-python scripts/train.py --preproc_dir_name=cs1000000-mf0 --epoch=5 --batch_size=128 --initial_lr=0.025 --final_lr=1e-4 --num_neg_samples=10 --window_size=5 --embed_dim=50
+python scripts/train.py --preproc_dir_name=cs0-mf5 --epoch=5 --batch_size=256 --initial_lr=0.025 --final_lr=1e-4 --num_neg_samples=5 --window_size=5 --embed_dim=100
 ```
 
 ```bash
--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -9,8 +9,10 @@ def build_parser():
 	parser = argparse.ArgumentParser(description="Dataset preprocessing script")
 	parser.add_argument("--corpus_size", type=int, default=0,
                      help="The number of words at the start of the corpus that are preprocessed. Default to 0. If set to 0 or negative, the entire corpus is preprocessed.")
-	parser.add_argument("--min_frequency", type=int, default=0, 
+	parser.add_argument("--min_frequency", type=int, default=0,
                      help="The minimum number of times a word need to appear to be included in the vocabulary. If set to 0 or negative, all words are included.")
+	parser.add_argument("--subsample_threshold", type=float, default=1e-5,
+                     help="Threshold for subsampling frequent words (Mikolov et al., 2013). Set to 0 to disable.")
 	return parser
 
 
@@ -24,24 +26,20 @@ def main():
         return
     os.mkdir(preproc_result_path)
     
-    word_list = []
     with open(DATASET_PATH) as f:
         word_list = f.read().split()
     
     if args.corpus_size > 0:
         word_list = word_list[:args.corpus_size]
     
+    freq_map = {}
+    for word in word_list:
+        freq_map[word] = freq_map.get(word, 0) + 1
+
     word_id_map = {}
     curr_word_id = 0
-    
-    if args.min_frequency > 1:
-        freq_map = {}
-        for word in word_list:
-            if word in freq_map:
-                freq_map[word] += 1
-            else:
-                freq_map[word] = 1
 
+    if args.min_frequency > 1:
         for word, freq in freq_map.items():
             if freq >= args.min_frequency:
                 word_id_map[word] = curr_word_id
@@ -50,15 +48,33 @@ def main():
         print(f"Ratio of vocabulary kept: {curr_word_id / len(freq_map)}")
 
     else:
-        curr_word_id = 0
         for word in word_list:
-            if word in word_id_map:
-                continue
-            word_id_map[word] = curr_word_id
-            curr_word_id += 1
+            if word not in word_id_map:
+                word_id_map[word] = curr_word_id
+                curr_word_id += 1
 
-    word_id_list = [word_id_map[word] for word in word_list if word in word_id_map]
+    # Subsampling of frequent words
+    total_vocab_words = sum(freq_map[w] for w in word_id_map)
+    rng = np.random.default_rng(42)
+    word_id_list = []
+    freq_discard_ctr = 0
+    subsample_discard_ctr = 0
+    
+    for word in word_list:
+        if word not in word_id_map:
+            freq_discard_ctr += 1
+            continue
+        if args.subsample_threshold > 0:
+            freq = freq_map[word] / total_vocab_words
+            keep_prob = np.sqrt(args.subsample_threshold / freq) + args.subsample_threshold / freq
+            if rng.random() > keep_prob:
+                subsample_discard_ctr += 1
+                continue
+        word_id_list.append(word_id_map[word])
     word_id_array = np.array(word_id_list)
+    
+    print(f"Ratio of words discarded due to low frequency: {freq_discard_ctr / total_vocab_words}")
+    print(f"Number of words discarded due to subsampling: {subsample_discard_ctr / total_vocab_words}")
 
     with open(f"{preproc_result_path}/word_id_map.pkl", "wb") as map_file:
         pickle.dump(word_id_map, map_file)
